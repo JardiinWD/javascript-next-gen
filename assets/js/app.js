3a0444de4abef3f9ed99d7903082c7ebb7ff1298
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-08-04 17:33:04 +0200

    Generato le card dinamicamente tramite js

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,9 +4,15 @@
 const menu = document.querySelector("#mobile-menu")
 /* Seleziono la mia lista non ordinata con tutti i list item */
 const menuLinks = document.querySelector(".navbar_menu")
-
 /* Verifica in console */
 console.log(menu, menuLinks);
+
+/* Seleziono il wrapper delle mie cards */
+const services = document.querySelector(".services_container")
+/* Verifica in console */
+console.log(services);
+
+
 
 /* Creazione di navbar di oggetto */
 const navbarLinks = [
@@ -28,26 +34,50 @@ const navbarLinks = [
 ]
 console.log(navbarLinks);
 
-
-
+/* Creazione dei dati dinamici delle cards */
+const servicesCardsData = [
+    /* 1°Card */
+    {
+        h2 : 'Experience Bliss',
+        p : 'Ai Powered Technology',
+        btn : 'Get Started'
+    },
+    /* 2°Card */
+    {
+        h2 : 'Are you Ready?',
+        p : 'Take the Leap',
+        btn : 'Get Started'
+    }
+]
+console.log(servicesCardsData);
 
 //#endregion
 
-//#region Funzioni
-
-//#endregion
 
 //#region Cicli
 
 /* Avvio il ciclo ForEach per la Navbar dinamica */
 navbarLinks.forEach(element => {
-    /* console.log(`${key} : ${element[key]}`); */
     let navbarLink = 
     `<li class="navbar_item">
         <a href="${element.href}" class="navbar_links">${element.name}</a>
     </li> 
     `
     menuLinks.insertAdjacentHTML('afterbegin', navbarLink)
+})
+
+servicesCardsData.forEach(element => {
+    console.log(element); // Verifica del mio oggetto
+    /* Creo la mia singola card dinamica */
+    let singleCard = 
+    `
+    <div class="services_card">
+        <h2>${element.h2}</h2>
+        <p>${element.p}</p>
+        <button>${element.btn}</button>
+    </div>   
+    `
+    services.insertAdjacentHTML('beforeend', singleCard)
 })
 
 
