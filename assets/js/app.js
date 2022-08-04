01a8a5ca0dc67148b9e25afac4415642fcb3be6f
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-08-04 16:48:55 +0200

    Generati link navbar dinamici

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,11 +8,51 @@ const menuLinks = document.querySelector(".navbar_menu")
 /* Verifica in console */
 console.log(menu, menuLinks);
 
+/* Creazione di navbar di oggetto */
+const navbarLinks = [
+    /* Link Home */
+    {
+        href : '/',
+        name : 'Home'
+    },
+    /* Link Tech */
+    {
+        href : '/tech.html',
+        name : 'Tech'
+    },
+    /* Link Products */
+    {
+        href : '/',
+        name : 'Products'
+    },
+]
+console.log(navbarLinks);
+
+
+
+
 //#endregion
 
 //#region Funzioni
 
 //#endregion
+
+//#region Cicli
+
+/* Avvio il ciclo ForEach per la Navbar dinamica */
+navbarLinks.forEach(element => {
+    /* console.log(`${key} : ${element[key]}`); */
+    let navbarLink = 
+    `<li class="navbar_item">
+        <a href="${element.href}" class="navbar_links">${element.name}</a>
+    </li> 
+    `
+    menuLinks.insertAdjacentHTML('afterbegin', navbarLink)
+})
+
+
+//#endregion
+
 
 //#region Eventi
 
